31d356bd1b401cdc9fb58ff1862ebe9c56fb0857
camrobjones <crjones94@googlemail.com>
2019-03-24 14:36:08 +0100

    portability

--- EDS/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def main_home(request):
-	return render(request, 'conrad/main_home.html')
-	
-def about(request):
-	return render(request, 'conrad/about.html')
-	
