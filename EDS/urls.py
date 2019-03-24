ab9b32b0c687c55cdfbf0e77605e7d7f02a49f97
camrobjones <crjones94@googlemail.com>
2019-03-24 14:29:21 +0100

    server migration 1

--- EDS/urls.py
+++ EDS/urls.py
@@ -29,22 +29,43 @@ urlpatterns = [
     path('', views.main_home, name = 'home'),
     path('about/', views.about, name = 'about'),
 
-    path('accounts/login/',  auth_views.LoginView.as_view(template_name='EDS/login.html'), name='login'),
-    path('accounts/reset/', auth_views.PasswordResetView.as_view(template_name='EDS/password_reset.html'),
+    path('accounts/login/',  auth_views.LoginView.as_view(
+        template_name='EDS/login.html'), name='login'),
+
+    path('accounts/reset/', auth_views.PasswordResetView.as_view(
+        template_name='EDS/password_reset.html'),
          name='password_reset'),
-    path('accounts/logout/',  auth_views.LogoutView.as_view(template_name='EDS/logout.html'), name='logout'),
+
+    path('accounts/logout/',  auth_views.LogoutView.as_view(
+        template_name='EDS/logout.html'), name='logout'),
+
     path('accounts/profile/', views.profile, name='profile'),
     path('accounts/signup/', views.signup, name='signup'),
 
     path('convert/', convert, name = 'lazysignup_convert'),
      #    include('lazysignup.urls')),
-    path('accounts/privacy_policy/', views.privacy_policy, name='privacy_policy'),
-    path('accounts/password/', views.change_password, name='change_password'),
-    path('password_reset/', auth_views.password_reset, name='password_reset'),
-    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
+    path('accounts/privacy_policy/', views.privacy_policy,
+     name='privacy_policy'),
+
+    path('accounts/password/', views.change_password,
+     name='change_password'),
+
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+            template_name='EDS/password_reset')),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'), 
+    name='password_reset_done'),
+
     path('reset/<uidb64>[0-9A-Za-z_\-]+/<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
+        auth_views.PasswordResetConfirmView.as_view( 
+        template_name='registration/password_reset_confirm.html'),
+    name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'),
+        name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
