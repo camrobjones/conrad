115368666fafe84d15c7e3fd0436fda05265b194
camrobjones <crjones94@googlemail.com>
2019-06-02 18:14:37 +0100

    ads & storage

--- ga/storage_utils.py
+++ ga/storage_utils.py
@@ -6,12 +6,12 @@ import boto
 from background_task import background
 import custom_storages
 
+
 def upload(pil_image, s3name):
     in_mem_file = io.BytesIO()
     pil_image.save(in_mem_file, format='PNG')
 
-
-    S3Bucket = 'eds-conrad'
+    S3Bucket = settings.AWS_STORAGE_BUCKET_NAME
     s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host='s3.eu-west-2.amazonaws.com')
     bucket = s3.get_bucket(S3Bucket)
     key = bucket.new_key('{}'.format(s3name))
@@ -19,13 +19,16 @@ def upload(pil_image, s3name):
     key.set_contents_from_file(in_mem_file)
     key.set_acl('public-read')
     
+
 def check_exists(x):
-	s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host='s3.eu-west-2.amazonaws.com')
-	try:
-		s3.Object('eds-conrad', 'media/' + x).load()
-		return True
-	except:
-		return False
+
+    s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY, host='s3.eu-west-2.amazonaws.com')
+    S3Bucket = settings.AWS_STORAGE_BUCKET_NAME
+    try:
+        s3.Object(S3Bucket, 'media/' + x).load()
+        return True
+    except:
+        return False
 
 def get_url(x):
-	return '//' + settings.AWS_S3_CUSTOM_DOMAIN + '/' + x
+    return '//' + settings.AWS_S3_CUSTOM_DOMAIN + '/' + x
